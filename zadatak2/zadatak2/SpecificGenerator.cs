84e1d284d92d6c5441b7be1e1df00126b119cdba
Mario Nižić <mnizic@foi.hr>
2021-07-14 12:31:50 +0200

    Implementacija generiranja zaporke koja sadrži samo mala slova

---
+++ zadatak2/zadatak2/SpecificGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadatak2
+{
+    class SpecificGenerator : PasswordGenerator
+    {
+        public override string generatePassword(int size)
+        {
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            for (int i = 0; i < size; i++)
+            {
+                passwordBuilder.Append(this.getRandomCharacter().ToString().ToLower());
+            }
+            return passwordBuilder.ToString();
+        }
+    }
+}
