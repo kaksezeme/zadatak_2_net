9981275102b54339dc2cf3fdb3f0cd76009a9075
Mario Nižić <mnizic@foi.hr>
2021-07-14 12:40:40 +0200

    Ispravak generatora

--- zadatak2/zadatak2/SpecificGenerator.cs
+++ zadatak2/zadatak2/SpecificGenerator.cs
@@ -6,12 +6,18 @@ namespace zadatak2
 {
     class SpecificGenerator : PasswordGenerator
     {
+
         public override string generatePassword(int size)
         {
             StringBuilder passwordBuilder = new StringBuilder();
-
+            string letter;
             for (int i = 0; i < size; i++)
             {
+                do
+                {
+                    letter = this.getRandomCharacter().ToString().ToLower();
+                } while (letter == "0" && letter == "1" && letter == "2" && letter == "3" && letter == "4" &&
+                    letter == "5" && letter == "6" && letter == "7" && letter == "8" && letter == "9");
                 passwordBuilder.Append(this.getRandomCharacter().ToString().ToLower());
             }
             return passwordBuilder.ToString();
