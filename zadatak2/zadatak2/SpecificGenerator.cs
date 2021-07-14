b67f775d4632d827c1050c16175920c668bccb1c
Mislav Jančić <mislav.jancic@protonmail.com>
2021-07-14 13:03:56 +0200

    Merge conflicts

---
+++ zadatak2/zadatak2/SpecificGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace zadatak2
+{
+    class SpecificGenerator : PasswordGenerator
+    {
+        public string ALLOWED_CHARS = "abcdefghijklmnoprstuvzxy";
+        public override string generatePassword(int size)
+        {
+            StringBuilder passwordBuilder = new StringBuilder();
+            for (int i = 0; i < size; i++)
+            {
+                var character = getRandomCharacter().ToString();
+
+                if (ALLOWED_CHARS.Contains(character) || ALLOWED_CHARS.ToUpper().Contains(character))
+                {
+                    passwordBuilder.Append(character);
+                }
+                else
+                {
+                    var newChar = int.Parse(character);
+                    passwordBuilder.Append((char)newChar);
+                }
+            }
+
+            
+
+            return passwordBuilder.ToString().ToLower();
+        }
+    }
+}
