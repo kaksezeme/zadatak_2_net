f82b9da379ffdf435dccc2573a2441432c8330c0
Boris Baštek <boris.bastek@gmail.com>
2021-07-14 13:01:31 +0200

    Fix

--- zadatak2/zadatak2/BasicPasswordGenerator.cs
+++ zadatak2/zadatak2/BasicPasswordGenerator.cs
@@ -6,9 +6,17 @@ namespace zadatak2
 {
     class BasicPasswordGenerator : PasswordGenerator
     {
+       
         public override string generatePassword(int size)
         {
-            throw new NotImplementedException();
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            for (int i = 0; i < size; i++)
+            {
+                passwordBuilder.Append(this.getRandomCharacter());
+            }
+            return passwordBuilder.ToString().ToLower();
         }
     }
+    
 }
